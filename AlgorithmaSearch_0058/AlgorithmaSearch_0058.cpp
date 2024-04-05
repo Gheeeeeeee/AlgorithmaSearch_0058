59681cf72f43f433e57a37e85ff6b40162eacfa4
gheeeeeeee <gheoputra55@gmail.com>
2024-04-05 15:20:52 +0700

    langkah pertama

--- AlgorithmaSearch_0058/AlgorithmaSearch_0058.cpp
+++ AlgorithmaSearch_0058/AlgorithmaSearch_0058.cpp
@@ -36,6 +36,13 @@ void LinearSearch()
 	char ch;
 	int ctr; // number of comparisons
 	int item;
+
+	do
+	{
+		// Accept the number to be searched
+		cout << "\nEnter the element you waant to search : "; //step 1
+		cin >> item;
+	}
 }
 
 
